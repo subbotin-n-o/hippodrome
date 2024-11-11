61332f736f3cfe062746460c23923742393f8222
nikitasubbotin <subbotin.n.o@gmail.com>
2024-11-11 21:53:51 +0400

    add C_CheckExcWhenFirstParamIsEmpty(), D_CheckExcMessageWhenFirstParamIsEmpty()

--- src/test/java/com/javarush/HorseTest.java
+++ src/test/java/com/javarush/HorseTest.java
@@ -1,6 +1,8 @@
 package com.javarush;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -20,11 +22,32 @@ public class HorseTest {
     @Test
     @DisplayName("Check exception message when first parameter is null")
     public void B_CheckExcMessageWhenFirstParamIsNull() {
-        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+        Exception exceptionMessage = assertThrows(IllegalArgumentException.class, () ->
                 new Horse(null, randomDate.getRandomSpeed(), randomDate.getRandomDistance()));
 
         String expectedMessage = "Name cannot be null.";
-        String actualMessage = exception.getMessage();
+        String actualMessage = exceptionMessage.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @ParameterizedTest
+    @DisplayName("Check exception when first parameter is empty")
+    @ValueSource(strings = { "", "  "})
+    public void C_CheckExcWhenFirstParamIsEmpty(String horseName) {
+        assertThrows(IllegalArgumentException.class, () ->
+                new Horse(horseName, randomDate.getRandomSpeed(), randomDate.getRandomDistance()));
+    }
+
+    @ParameterizedTest
+    @DisplayName("Check exception message when first parameter is empty")
+    @ValueSource(strings = { "", "  "})
+    public void D_CheckExcMessageWhenFirstParamIsEmpty(String horseName) {
+        Exception exceptionMessage = assertThrows(IllegalArgumentException.class, () ->
+                new Horse(horseName, randomDate.getRandomSpeed(), randomDate.getRandomDistance()));
+
+        String expectedMessage = "Name cannot be blank.";
+        String actualMessage = exceptionMessage.getMessage();
 
         assertEquals(expectedMessage, actualMessage);
     }
