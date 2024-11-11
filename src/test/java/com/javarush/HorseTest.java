ebb499b73dbedc5d48bf6b03f34df9e741abc633
nikitasubbotin <subbotin.n.o@gmail.com>
2024-11-11 21:27:15 +0400

    add B_CheckExcMessageWhenFirstParamIsNull()

--- src/test/java/com/javarush/HorseTest.java
+++ src/test/java/com/javarush/HorseTest.java
@@ -2,6 +2,7 @@ package com.javarush;
 
 import org.junit.jupiter.api.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @TestMethodOrder(MethodOrderer.MethodName.class)
@@ -14,5 +15,17 @@ public class HorseTest {
     public void A_CheckExcWhenFirstParamIsNull() {
         assertThrows(IllegalArgumentException.class, () ->
                 new Horse(null, randomDate.getRandomSpeed(), randomDate.getRandomDistance()));
+    }
+
+    @Test
+    @DisplayName("Check exception message when first parameter is null")
+    public void B_CheckExcMessageWhenFirstParamIsNull() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+                new Horse(null, randomDate.getRandomSpeed(), randomDate.getRandomDistance()));
+
+        String expectedMessage = "Name cannot be null.";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
     }
 }
