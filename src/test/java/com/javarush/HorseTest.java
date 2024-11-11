4efc6290fcfa645496bd1ac7f9e8d84ce4002656
nikitasubbotin <subbotin.n.o@gmail.com>
2024-11-11 21:59:48 +0400

    add E_CheckExcWhenSecondParamNegativeNumber()

--- src/test/java/com/javarush/HorseTest.java
+++ src/test/java/com/javarush/HorseTest.java
@@ -16,14 +16,14 @@ public class HorseTest {
     @DisplayName("Check exception when first parameter is null")
     public void A_CheckExcWhenFirstParamIsNull() {
         assertThrows(IllegalArgumentException.class, () ->
-                new Horse(null, randomDate.getRandomSpeed(), randomDate.getRandomDistance()));
+                new Horse(null, randomDate.getRandomPositiveNumber(), randomDate.getRandomPositiveNumber()));
     }
 
     @Test
     @DisplayName("Check exception message when first parameter is null")
     public void B_CheckExcMessageWhenFirstParamIsNull() {
         Exception exceptionMessage = assertThrows(IllegalArgumentException.class, () ->
-                new Horse(null, randomDate.getRandomSpeed(), randomDate.getRandomDistance()));
+                new Horse(null, randomDate.getRandomPositiveNumber(), randomDate.getRandomPositiveNumber()));
 
         String expectedMessage = "Name cannot be null.";
         String actualMessage = exceptionMessage.getMessage();
@@ -36,7 +36,7 @@ public class HorseTest {
     @ValueSource(strings = { "", "  "})
     public void C_CheckExcWhenFirstParamIsEmpty(String horseName) {
         assertThrows(IllegalArgumentException.class, () ->
-                new Horse(horseName, randomDate.getRandomSpeed(), randomDate.getRandomDistance()));
+                new Horse(horseName, randomDate.getRandomPositiveNumber(), randomDate.getRandomPositiveNumber()));
     }
 
     @ParameterizedTest
@@ -44,11 +44,18 @@ public class HorseTest {
     @ValueSource(strings = { "", "  "})
     public void D_CheckExcMessageWhenFirstParamIsEmpty(String horseName) {
         Exception exceptionMessage = assertThrows(IllegalArgumentException.class, () ->
-                new Horse(horseName, randomDate.getRandomSpeed(), randomDate.getRandomDistance()));
+                new Horse(horseName, randomDate.getRandomPositiveNumber(), randomDate.getRandomPositiveNumber()));
 
         String expectedMessage = "Name cannot be blank.";
         String actualMessage = exceptionMessage.getMessage();
 
         assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    @DisplayName("Check exception when second parameter negative number")
+    public void E_CheckExcWhenSecondParamNegativeNumber() {
+        assertThrows(IllegalArgumentException.class, () ->
+                new Horse(randomDate.getRandomName(), randomDate.getRandomNegativeNumber(), randomDate.getRandomPositiveNumber()));
     }
 }
