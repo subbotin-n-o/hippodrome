a7e24bbb6d3e236cda2e0ab52b0205a757667809
nikitasubbotin <subbotin.n.o@gmail.com>
2024-11-11 23:21:32 +0400

    add checkExcWhenParamConstrEmptyList()

--- src/test/java/com/javarush/HippodromeTest.java
+++ src/test/java/com/javarush/HippodromeTest.java
@@ -2,6 +2,10 @@ package com.javarush;
 
 import org.junit.jupiter.api.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -13,10 +17,33 @@ public class HippodromeTest extends BaseTest {
     }
 
     @Test
-    @DisplayName("Check exception when parameter constructors is null")
+    @DisplayName("Check exception, when parameter of constructor is null")
     @Order(1)
     public void checkExcWhenParamConstrIsNull() {
         assertThrows(IllegalArgumentException.class, () ->
                 new Hippodrome(null));
+    }
+
+    @Test
+    @DisplayName("Check exception message, when parameter of constructor is null")
+    @Order(2)
+    public void checkExcMessageWhenFirstParamIsNull() {
+        Exception exceptionMessage = assertThrows(IllegalArgumentException.class, () ->
+                new Hippodrome(null));
+
+        String expectedMessage = "Horses cannot be null.";
+        String actualMessage = exceptionMessage.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    @DisplayName("Check exception, when parameter of constructor empty List")
+    @Order(3)
+    public void checkExcWhenParamConstrEmptyList() {
+        List<Horse> emptyList = new ArrayList<>();
+
+        assertThrows(IllegalArgumentException.class, () ->
+                new Hippodrome(emptyList));
     }
 }
