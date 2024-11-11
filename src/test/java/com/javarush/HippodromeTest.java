0cb5c4a016d392bf907dd5df3cc01ecd18402dc3
nikitasubbotin <subbotin.n.o@gmail.com>
2024-11-11 23:09:11 +0400

    add checkExcWhenParamConstrIsNull()

--- src/test/java/com/javarush/HippodromeTest.java
+++ src/test/java/com/javarush/HippodromeTest.java
@@ -1,4 +1,22 @@
 package com.javarush;
 
-public class HippodromeTest {
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class HippodromeTest extends BaseTest {
+
+    @BeforeEach
+    public void initGeneratorRandomDate() {
+        randomDate = new GeneratorRandomDate();
+    }
+
+    @Test
+    @DisplayName("Check exception when parameter constructors is null")
+    @Order(1)
+    public void checkExcWhenParamConstrIsNull() {
+        assertThrows(IllegalArgumentException.class, () ->
+                new Hippodrome(null));
+    }
 }
