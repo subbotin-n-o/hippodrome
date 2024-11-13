3506654aed440a224f1085f993ee54600f05ecf3
nikitasubbotin <subbotin.n.o@gmail.com>
2024-11-13 21:50:37 +0400

    add mainTimeout()

--- src/test/java/com/javarush/MainTest.java
+++ src/test/java/com/javarush/MainTest.java
@@ -1,4 +1,16 @@
 package com.javarush;
 
-public class MainTest {
+import org.junit.jupiter.api.*;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public class MainTest extends BaseTest{
+
+    @Test
+    @DisplayName("Check timeout method main()")
+    @Timeout(value = 22, unit = SECONDS)
+    @Disabled
+    public void mainTimeout() throws Exception {
+        Main.main(new String[] {});
+    }
 }
